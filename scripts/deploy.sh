e19adcbd4838fd209e92a1badb23018470849038
David Ansermino <david.ansermino@mail.utoronto.ca>
2018-09-21 14:22:31 -0400

    Added git user config

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
+git config user.email "david@chainsafe.io"
+git config user.name "David Ansermino"
+
 git remote add ethereum https://github.com/ethereum/tests
 git pull ethereum develop --rebase
 git commit -am 'CircleCi Merge with upstream'
