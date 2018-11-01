debcd7824523ac9144792ca0a87927a15c70513e
David Ansermino <david@chainsafe.io>
2018-11-01 20:52:24 +0100

    David/merkle tests (#3)
    Restored existing tests and added new ones

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,5 +5,7 @@ git config user.email "david@chainsafe.io"
 git config user.name "David Ansermino"
 
 git remote add ethereum https://github.com/ethereum/tests
-git pull ethereum develop --rebase
+
+git pull ethereum develop
 git diff-index --quiet HEAD || (git commit -am 'CircleCi Merge with upstream' && git push origin develop)
+
