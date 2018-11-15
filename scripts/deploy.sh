32024cf66eb625cbfe8b34d47868067cd4ea6519
David Ansermino <david@chainsafe.io>
2018-11-15 01:46:43 +0100

    Merkle test updates (#4)
    Updated to new path format

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,6 @@ git config user.email "david@chainsafe.io"
 git config user.name "David Ansermino"
 
 git remote add ethereum https://github.com/ethereum/tests
-
 git pull ethereum develop
 git diff-index --quiet HEAD || (git commit -am 'CircleCi Merge with upstream' && git push origin develop)
 
